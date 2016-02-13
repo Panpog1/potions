562324742d6b539ed6a89751c6e493d68cfe82f5
Patrick McDonough <panpog@gmail.com>
2016-02-13 14:47:18 -0800

    Refactor Parser into its own class

--- src/io/github/panpog1/potions/Cauldron.java
+++ src/io/github/panpog1/potions/Cauldron.java
@@ -1,14 +1,9 @@
 package io.github.panpog1.potions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Cauldron {
-	private static String[] constIdgNames = { "Ae", "T", "H", "Ah", "S" };
-	private static final String packageName = Cauldron.class.getPackage().getName();
 
 	public Set<Compound> idgs = new HashSet<Compound>();
 	public boolean h;
@@ -27,7 +22,7 @@ public class Cauldron {
 	}
 
 	void add(String next) throws CompoundParseException {
-		add(parse(next));
+		add(Parser.parse(next));
 	}
 
 	void add(Compound next) {
@@ -42,132 +37,5 @@ public class Cauldron {
 				}
 			}
 		}
-	}
-
-	static Compound parse(String s) throws CompoundParseException {
-		return parse(s, s, 0);
-	}
-
-
-	private static Compound parse(String all, String s, int offset) throws CompoundParseException {
-		// T S and H are in Add
-		if (s.trim().isEmpty())
-			throw new CompoundParseException(all, offset);
-		s = numbersToLetters(s.trim());
-		if (s == null)
-			throw new CompoundParseException(all, offset);
-		if (s.startsWith("U")) {
-			Compound inner = parse(all, s.substring(1), offset + 1);
-			inner.applyU();
-			return inner;
-		}
-		if (s.startsWith("E")) {
-			Compound inner = parse(all, s.substring(1), offset + 1);
-			return new E(inner);
-		}
-		if (s.startsWith("\"") && s.endsWith("\"")) {
-			offset++;
-			if (s.length() == 1)
-				throw new CompoundParseException(all, offset);
-			s = s.substring(1, s.length() - 1);
-			if (s.contains("\""))
-				throw new CompoundParseException(all, s.indexOf("\""));
-			if (s.contains(" "))
-				throw new CompoundParseException(all, s.indexOf(" "));
-			return new Base(s);
-		}
-		if (s.startsWith("R")) {
-			Compound inner = parse(all, s.substring(1), offset + 1);
-			return new R(inner);
-		}
-		if (s.startsWith("If(")) {
-			return parseIf(all, s, offset);
-		}
-		return checkConstIdgNames(all, s, offset);
-	}
-
-	// TODO: Make offset more accurate in CompoundParseException thrown in this
-	// method
-	static Compound parseIf(String all, String s, int offset) throws CompoundParseException {
-		if (!s.endsWith(")"))
-			throw new CompoundParseException(all, offset + s.length());
-		offset += 3;
-		s = s.substring(3, s.length() - 1);
-		List<String> tokens = new ArrayList<String>(
-				Arrays.asList(s.replace("(", ",(,").replace(")", ",),").replace("\"", ",\",").split(",")));
-		while (tokens.remove(""));// remove all empty strings
-		List<Compound> parts = parseParts(all, offset, tokens);
-		// make conditions an array containing the contents of parts except for the
-		// last element
-		final List<Compound> subList = parts.subList(0, parts.size() - 1);
-		Compound[] conditions = new Compound[subList.size()];
-		subList.toArray(conditions);
-		return new If(conditions, parts.get(parts.size() - 1));
-	}
-
-	private static List<Compound> parseParts(String all, int offset, List<String> tokens)
-			throws CompoundParseException {
-		List<Compound> parts = new ArrayList<Compound>();
-		System.out.println(tokens);
-		for (int i = 0; i < tokens.size(); i++) {
-			String partString = "";
-			if (tokens.get(i).equals("\"")) {
-				do {
-					if (i >= tokens.size()) {
-						throw new CompoundParseException(all, offset);
-					}
-					partString += tokens.get(i);
-					i++;
-				} while (!tokens.get(i).equals("\""));
-				partString += tokens.get(i);
-				System.out.println(partString);
-				parts.add(parse(partString));
-			}
-		}
-		return parts;
-	}
-
-	private static Compound checkConstIdgNames(String all, String s, int offset)
-			throws CompoundParseException {
-		for (String constIdgName : constIdgNames) {
-			if (s.startsWith(constIdgName)) {
-				if (!s.equals(constIdgName)) {
-					throw new CompoundParseException(all, offset + constIdgName.length());
-				}
-				try {
-					Class<?> obj;
-					obj = Class.forName(packageName + "." + constIdgName);
-					@SuppressWarnings("unchecked")
-					Class<Compound> clazz = (Class<Compound>) obj;
-					return clazz.getConstructor().newInstance();
-				} catch (Exception e) {
-					throw new AssertionError(e);
-				}
-			}
-		}
-		throw new CompoundParseException(all, offset);
-	}
-
-	static String numbersToLetters(String s) {
-		if (Character.isDigit(s.charAt(0))) {
-			boolean failure = true;
-			for (int i = 1; i < s.length(); i++) {
-				if (!Character.isDigit(s.charAt(i))) {
-					String prefix = "";
-					int times = Integer.parseInt(s.substring(0, i)) - 1;
-					s = s.substring(i);
-					String first = s.substring(0, 1);
-					for (int j = 0; j < times; j++) {
-						prefix += first;
-					}
-					failure = false;
-					s = prefix + s;
-					break;
-				}
-			}
-			if (failure)
-				return null;
-		}
-		return s;
 	}
 }
---
+++ src/io/github/panpog1/potions/Parser.java
@@ -0,0 +1,143 @@
+package io.github.panpog1.potions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Parser {
+	static String[] constIdgNames = { "Ae", "T", "H", "Ah", "S" };
+	static final String packageName = Parser.class.getPackage().getName();
+
+	String all;
+
+	static Compound parse(String s) throws CompoundParseException {
+		return new Parser(s).parse(s, 0);
+	}
+
+	private Parser(String all) {
+		this.all = all;
+	}
+
+	Compound parse(String s, int offset) throws CompoundParseException {
+		// T S and H are in Add
+		if (s.trim().isEmpty())
+			throw new CompoundParseException(all, offset);
+		s = numbersToLetters(s.trim());
+		if (s == null)
+			throw new CompoundParseException(all, offset);
+		if (s.startsWith("U")) {
+			Compound inner = parse(s.substring(1), offset + 1);
+			inner.applyU();
+			return inner;
+		}
+		if (s.startsWith("E")) {
+			Compound inner = parse(s.substring(1), offset + 1);
+			return new E(inner);
+		}
+		if (s.startsWith("\"") && s.endsWith("\"")) {
+			offset++;
+			if (s.length() == 1)
+				throw new CompoundParseException(all, offset);
+			s = s.substring(1, s.length() - 1);
+			if (s.contains("\""))
+				throw new CompoundParseException(all, s.indexOf("\""));
+			if (s.contains(" "))
+				throw new CompoundParseException(all, s.indexOf(" "));
+			return new Base(s);
+		}
+		if (s.startsWith("R")) {
+			Compound inner = parse(s.substring(1), offset + 1);
+			return new R(inner);
+		}
+		if (s.startsWith("If(")) {
+			return parseIf(s, offset);
+		}
+		return checkConstIdgNames(s, offset);
+	}
+
+	// TODO: Make offset more accurate in CompoundParseException thrown in this
+	// method
+	Compound parseIf(String s, int offset) throws CompoundParseException {
+		if (!s.endsWith(")"))
+			throw new CompoundParseException(all, offset + s.length());
+		offset += 3;
+		s = s.substring(3, s.length() - 1);
+		List<String> tokens = new ArrayList<String>(
+				Arrays.asList(s.replace("(", ",(,").replace(")", ",),").replace("\"", ",\",").split(",")));
+		while (tokens.remove(""));// remove all empty strings
+		List<Compound> parts = parseParts(offset, tokens);
+		// make conditions an array containing the contents of parts except for the
+		// last element
+		final List<Compound> subList = parts.subList(0, parts.size() - 1);
+		Compound[] conditions = new Compound[subList.size()];
+		subList.toArray(conditions);
+		return new If(conditions, parts.get(parts.size() - 1));
+	}
+
+	private List<Compound> parseParts(int offset, List<String> tokens)
+			throws CompoundParseException {
+		List<Compound> parts = new ArrayList<Compound>();
+		System.out.println(tokens);
+		for (int i = 0; i < tokens.size(); i++) {
+			String partString = "";
+			if (tokens.get(i).equals("\"")) {
+				do {
+					if (i >= tokens.size()) {
+						throw new CompoundParseException(all, offset);
+					}
+					partString += tokens.get(i);
+					i++;
+				} while (!tokens.get(i).equals("\""));
+				partString += tokens.get(i);
+				System.out.println(partString);
+				parts.add(parse(partString));
+			}
+		}
+		return parts;
+	}
+
+	private Compound checkConstIdgNames(String s, int offset)
+			throws CompoundParseException {
+		for (String constIdgName : constIdgNames) {
+			if (s.startsWith(constIdgName)) {
+				if (!s.equals(constIdgName)) {
+					throw new CompoundParseException(all, offset + constIdgName.length());
+				}
+				try {
+					Class<?> obj;
+					obj = Class.forName(packageName + "." + constIdgName);
+					@SuppressWarnings("unchecked")
+					Class<Compound> clazz = (Class<Compound>) obj;
+					return clazz.getConstructor().newInstance();
+				} catch (Exception e) {
+					throw new AssertionError(e);
+				}
+			}
+		}
+		throw new CompoundParseException(all, offset);
+	}
+
+	String numbersToLetters(String s) {
+		if (Character.isDigit(s.charAt(0))) {
+			boolean failure = true;
+			for (int i = 1; i < s.length(); i++) {
+				if (!Character.isDigit(s.charAt(i))) {
+					String prefix = "";
+					int times = Integer.parseInt(s.substring(0, i)) - 1;
+					s = s.substring(i);
+					String first = s.substring(0, 1);
+					for (int j = 0; j < times; j++) {
+						prefix += first;
+					}
+					failure = false;
+					s = prefix + s;
+					break;
+				}
+			}
+			if (failure)
+				return null;
+		}
+		return s;
+	}
+
+}
